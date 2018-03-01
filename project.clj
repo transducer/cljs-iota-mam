528f5889966044e37c150f68e7cb06c9cec60fc9
Erwin Rooijakkers <erwin@rooijakkers.software>
2018-03-01 11:58:56 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-iota-mam "1.0.1"
+(defproject cljs-iota-mam "1.0.2"
   :description "ClojureScript API for IOTA Masked Authenticated Messaging (MAM) JavaScript API"
 
   :url            "https://github.com/transducer/cljs-iota-mam"
